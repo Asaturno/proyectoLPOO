0eba56d97e31379e0463ce9843637b304ff21de5
lenoo <lenoo@lenovo-PC>
2022-06-08 20:44:36 -0500

    model

--- Trivia/src/modelo/Categoria.java
+++ Trivia/src/modelo/Categoria.java
@@ -1,24 +1,46 @@
 package modelo;
+import java.util.List;
 
-public class Categoria {
+public class Categoria implements CategoriasDao{
 	
-	private String[] cadena_categorias;
+	private String nomCategoria;
 
-	public String[] getCadena_categorias() {
-		return cadena_categorias;
+	public String getNomCategoria() {
+		return nomCategoria;
 	}
-	
-	public void addCategoria(String categoria) {
-		for (int i=0; i<100; i++) {
-			if(this.cadena_categorias[i]==null) {
-				this.cadena_categorias[i]=categoria;
-				break;
-			}
-		}
+
+	public void setNomCategoria(String nomCategoria) {
+		this.nomCategoria = nomCategoria;
 	}
-	
-	
-	
-	
+
+	@Override
+	public void addCategoria(Categoria categoria) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean readCategoria(Categoria categoria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Categoria> listaCategorias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCategoria(Categoria categoria) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCategoria(Categoria categoria1, Categoria categoria2) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
