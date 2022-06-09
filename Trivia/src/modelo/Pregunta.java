17cfaea1c1d5d59f992a458f90990940f6520691
lenoo <lenoo@lenovo-PC>
2022-06-08 23:17:46 -0500

    modelo pregunta

--- Trivia/src/modelo/Pregunta.java
+++ Trivia/src/modelo/Pregunta.java
@@ -9,6 +9,7 @@ public class Pregunta implements PreguntaDao{
 	private Categoria categoria;
 	private ArrayList<Tema> temas;
 	private List<String> preguntas;
+	private List<String> respuestas;
 	
 	public String getPregunta() {
 		return pregunta;
@@ -22,26 +23,31 @@ public class Pregunta implements PreguntaDao{
 	}
 	@Override
 	public void addPregunta(Pregunta pregunta) {
-		
+		preguntas.add(pregunta.pregunta);
+		respuestas.add(pregunta.respuesta);
 	}
 	@Override
 	public boolean readPregunta(Pregunta pregunta) {
-		// TODO Auto-generated method stub
+		if(preguntas.contains(pregunta.pregunta)) {
+			return true;
+		}
 		return false;
 	}
 	@Override
-	public List<String> readTotal() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
 	public void deletePregunta(Pregunta pregunta) {
-		// TODO Auto-generated method stub
-		
+		preguntas.remove(pregunta.pregunta);
 	}
 	@Override
 	public void updatePregunta(Pregunta pregunta1, Pregunta pregunta2) {
-		// TODO Auto-generated method stub
-		
+		preguntas.remove(pregunta1.pregunta);
+		preguntas.add(pregunta2.pregunta);
+	}
+	@Override
+	public List<String> totalPreguntas() {
+		return preguntas;
+	}
+	@Override
+	public List<String> totalRespuestas() {
+		return respuestas;
 	}
 }
--- Trivia/src/modelo/PreguntaDao.java
+++ Trivia/src/modelo/PreguntaDao.java
@@ -5,7 +5,8 @@ import java.util.List;
 public interface PreguntaDao {
 	public void addPregunta(Pregunta pregunta);
 	public boolean readPregunta(Pregunta pregunta);
-	public List<String> readTotal();
+	public List<String> totalPreguntas();
+	public List<String> totalRespuestas();
 	public void deletePregunta(Pregunta pregunta);
 	public void updatePregunta(Pregunta pregunta1, Pregunta pregunta2);
 }
